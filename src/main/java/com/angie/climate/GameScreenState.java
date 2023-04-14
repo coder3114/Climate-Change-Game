000c1f2bbff9a3614df16d9c334ca71728059f6f
Enqi Yang <qiangie.yang@gmail.com>
2023-04-14 22:47:01 +0100

    Create main game screen

---
+++ src/main/java/com/angie/climate/GameScreenState.java
@@ -0,0 +1,36 @@
+package com.angie.climate;
+
+import javax.swing.*;
+
+public class GameScreenState {
+    private JButton iAmReadyToButton;
+    private JSlider slider2;
+    private JSlider slider3;
+    private JSlider slider4;
+    private JSlider slider5;
+    private JPanel mainPanel;
+
+    public JButton getIAmReadyToButton() {
+        return iAmReadyToButton;
+    }
+
+    public JSlider getSlider2() {
+        return slider2;
+    }
+
+    public JSlider getSlider3() {
+        return slider3;
+    }
+
+    public JSlider getSlider4() {
+        return slider4;
+    }
+
+    public JSlider getSlider5() {
+        return slider5;
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
